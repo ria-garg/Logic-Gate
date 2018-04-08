dc4f200630cf9186eba7e5aec610a4cabb0140c3
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:37:29 -0400

    Update ExceptionLogicParameters.java

--- ExceptionLogicParameters.java
+++ ExceptionLogicParameters.java
@@ -1,1 +1,45 @@
+public class ExceptionLogicParameters extends RuntimeException{
+//---------------------------------------------------------------------------------------------------------------------------
+  
+   // fields
+   private boolean inputsRelated;
+   private int expected;
+   private int found;
+  
+   // constuctor
+   public ExceptionLogicParameters(boolean inputsRelated, int expected, int found){
+     this.inputsRelated = inputsRelated;
+     this.expected = expected;
+     this.found = found;
+   }
+   
+   // for formatting
+   @Override public String toString(){
+     String toString = String.format("expected %d, but found %d",expected,found);
+     return toString;
+   }
+  
+   // getters
+   public boolean getInputsRelated(){
+      return this.inputsRelated;
+   }
+   public int getExpected(){
+      return this.expected;
+   }
+   public int getFound(){
+      return this.found;
+   }
 
+   // setters
+   public void setInputsRelated(boolean inputsRelated){
+      this.inputsRelated = inputsRelated;
+   }
+   public void setExpected(int expected){
+      this.expected = expected;
+   }
+   public void setFound(int found){
+      this.found = found;
+   }
+  
+//---------------------------------------------------------------------------------------------------------------------------
+}
