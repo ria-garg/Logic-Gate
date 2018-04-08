1fed837a7a13a4272e3e22a4a89fdcb8788b36a1
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:39:42 -0400

    Update GateNand.java

--- GateNand.java
+++ GateNand.java
@@ -1,68 +1,53 @@
-import java.util.*;
-
-public class GateNand extends Gate{
+public class Wire{
 //-------------------------------------------------------------------------------------------------------------------------------
-   
-   // constuctor
-   public GateNand(List<Wire> ins, Wire output){
-     super("Nand",ins,output); // gate constuctor
-   }
-  
+	
+   // fields
+   private Signal signal;
+	private String name;
+
+   // constructor
+	public Wire(String name){
+		this.name = name; // sets name
+		this.signal = Signal.X; // sets value of signal to an unkown value
+	}
+
+	@Override
+	public String toString(){
+		return (name + ":" + signal.toString());
+	}
+
    // equality check
-   @Override public boolean equals(Object other){
+	@Override
+	public boolean equals(Object other){
+		
+   boolean result = false; // just to initalize, this value may change later as it goes thru the if else  
+      
+      if (this == other){
+         result = true;
+      }
+      else if (other instanceof Wire){
+         Wire o = (Wire)other;
+         result = (o.getSignal() == signal && o.getName().equals(name));
+      }
+      
+   return result;   
+	}
    
-     if (other instanceof GateNand){
-       GateNand nAndGate = (GateNand) other;
-       return (super.equals(nAndGate));
-     }
-     else{
-        return false;
-     }
-   }
+   // getters
+	public Signal getSignal(){
+		return signal;
+	}
+	public String getName(){
+		return name;
+	}
+
+   // setters
+	public void setSignal(Signal signal){
+		this.signal = signal;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
    
-   // LO if all inputs are high, otherwise HI
-   // propagate method, seeing how it respinds to different input signals
-   @Override public boolean propagate(){
-     
-      Signal x = Signal.LO;
-     
-      int countHI = 0; 
-      int countX = 0;
-      int countLO = 0;
-     
-      // adding the number of times the HI or LO is signaled, total number at the end of loop
-      for (Wire i : getInputs()){
-        if (i.getSignal() == Signal.HI){
-           countHI++;
-        }
-        else if (i.getSignal() == Signal.X){
-           countX++;
-        }
-        else if(i.getSignal() == Signal.LO){
-           countLO++;
-        }
-      }
-      
-      // logic for the specfic gate
-      if (countLO > 0){
-         x = Signal.LO;
-      }
-      else if (countHI>0 && countX == 0){
-         x = Signal.HI;
-      }
-      
-      boolean a = true; // intializing value for the returned value, will change varying depending on the statement it goes thru
-      
-      if (getOutput().getSignal() == (x)){ // comparing signal values
-         a = false;
-         getOutput().setSignal(x); // setting the value of check as this signal
-      } 
-      else if (getOutput().getSignal() != (x)){ // comparing signal values
-         a= true;
-      }
-          
-   return a;
-   }
-  
 //-------------------------------------------------------------------------------------------------------------------------------
 }
