801bb5788118f53c5ce0df929ce9f517a0095918
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:42:36 -0400

    Update GateNot.java

--- GateNot.java
+++ GateNot.java
@@ -1,32 +1,40 @@
-import java.util.List;
+import java.util.*;
 
-public class GateNot extends Gate {
-
-	public GateNot(String name, List<Wire> ins, Wire out) throws RuntimeException {
-		super("NOT", ins, out);
-	}
-
-	@Override
-	public boolean propagate() {
-		boolean gate = true;
-		Wire w = (Wire) getInputs().get(0);
-		Signal s = w.getSignal();
-		s = s.invert();
-		if(getOutput().getSignal().equals(s)) {
-			gate = false;
-		}
-		getOutput().setSignal(s);
-		return gate;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		boolean path = false;
-		if (other instanceof GateNot) {
-			GateNot g = (GateNot) other;
-			path = super.equals(g);
-		}
-		return path;
-	}
-
+public class GateNot extends Gate{
+//---------------------------------------------------------------------------------------------------------------------------
+   
+   // constructor
+   public GateNot(Wire input, Wire output){
+      super("Not",Arrays.asList(new Wire[]{input}),output);
+   }
+   
+   // equality check 
+   @Override public boolean equals(Object other){
+      boolean check = false;
+      if (other instanceof GateAnd){
+         GateAnd g = (GateAnd) other;
+         check = super.equals(g);
+      }
+   return check;
+   }
+  
+   // inverts it's one value
+   // true if signal change, false otherwise
+   @Override public boolean propagate(){
+    
+      Signal s = w.getSignal();
+      Wire w = getInputs().get(0);
+      
+      s = s.invert(); // inverting the signal, will change unless it's X
+      
+      // determining if signal has changed
+      if (getOutput().getSignal().equals(s)){
+         getOutput().setSignal(s); // setting for later testing
+         return false;
+      }
+      
+   return true;
+   }
+      
+//---------------------------------------------------------------------------------------------------------------------------
 }
