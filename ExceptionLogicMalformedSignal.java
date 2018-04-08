b006fb545e7eae42d294cf046ce4f0231e782a32
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:37:45 -0400

    Update ExceptionLogicMalformedSignal.java

--- ExceptionLogicMalformedSignal.java
+++ ExceptionLogicMalformedSignal.java
@@ -1,1 +1,36 @@
+public class ExceptionLogicMalformedSignal extends RuntimeException{
+//--------------------------------------------------------------------------------------------------------------------------
+ 
+   // fields
+   private char bad;
+   private String msg;
+  
+   // constuctor
+   public ExceptionLogicMalformedSignal(char bad, String msg){
+      this.bad = bad;
+      this.msg = msg;
+   }
+   
+   // formatting purposes
+   @Override public String toString(){
+      return msg;
+   }
+  
+   // getters
+   public char getBad(){
+      return this.bad;
+   }
+   public String getMsg(){
+      return this.msg;
+   }
 
+   // setters
+   public void setBad(char bad){
+      this.bad = bad;
+   }
+   public void setMsg(String msg){
+      this.msg = msg;
+   }
+
+//--------------------------------------------------------------------------------------------------------------------------
+}
