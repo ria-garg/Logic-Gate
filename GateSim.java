49c0dd6af8a81d3d240d735d1b93ac69bb32c407
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:42:08 -0400

    Update GateSim.java

--- GateSim.java
+++ GateSim.java
@@ -1,1 +1,22 @@
+import java.util.*;
+import java.io.*;
 
+public class GateSim{
+//---------------------------------------------------------------------------------------------------------------------------
+  
+  public static void main(String[] args) throws Exception{
+    
+    try{
+      Circuit myCircuit = new Circuit(args[0]);
+      myCircuit.feed(args[1]);
+      myCircuit.propagate();
+      String str = Signal.toString(myCircuit.read());
+      System.out.println(str);
+    }
+    catch (Exception e){
+      System.out.println("couldn't perform simulation");
+    }
+  }
+  
+//---------------------------------------------------------------------------------------------------------------------------
+}
