59e1df254abe60bfb5c278d47082244f102205d9
ria-garg <35336023+ria-garg@users.noreply.github.com>
2018-04-08 09:41:30 -0400

    Update Logic.java

--- Logic.java
+++ Logic.java
@@ -8,13 +8,13 @@ public interface Logic{
 
    public abstract void feed(String inSignals); // accepts a string representation of signals
 
-	public abstract boolean propagate(); // performs the logic and generates outputs
+   public abstract boolean propagate(); // performs the logic and generates outputs
 
-	public abstract List<Signal> read(); // reads the output signals and creates a new List
+   public abstract List<Signal> read(); // reads the output signals and creates a new List
 
-	public abstract List<Signal> inspect(List<Signal> inputs); // combination of feeding, propagating, and reading
+   public abstract List<Signal> inspect(List<Signal> inputs); // combination of feeding, propagating, and reading
 
-	public abstract String inspect(String input); // combination of feeding, propagating, and reading
+   public abstract String inspect(String input); // combination of feeding, propagating, and reading
      
 //------------------------------------------------------------------------------------------------------------------------------
 }
