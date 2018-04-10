85763f64e79c6f95802a6e521cb68101d0019dd4
compi3n3rd <35336251+compi3n3rd@users.noreply.github.com>
2018-04-09 20:58:25 -0400

    Update ExceptionLogicMalformedSignal.java

--- ExceptionLogicMalformedSignal.java
+++ ExceptionLogicMalformedSignal.java
@@ -1,4 +1,4 @@
-public class ExceptionLogicMalformedSignal extends RuntimeException{
+public class ExceptionLogicMalformedSignal{
 //--------------------------------------------------------------------------------------------------------------------------
  
    // fields
